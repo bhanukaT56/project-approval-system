07699d5661be5a929f4bed146155c2721528b378
bhanukaT56 <itsbhanukat56@gmail.com>
2026-04-16 20:27:19 +0530

    Configure relational mapping between Project and UserProfile in DbContext

--- BlindMatchPAS/Data/ApplicationDbContext.cs
+++ BlindMatchPAS/Data/ApplicationDbContext.cs
@@ -6,12 +6,34 @@ namespace BlindMatchPAS.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        public ApplicationDbContext(
+            DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
 
         public DbSet<Project> Projects { get; set; }
         public DbSet<UserProfile> UserProfiles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // Configure Project -> StudentProfile relationship
+            builder.Entity<Project>()
+                .HasOne(p => p.StudentProfile)
+                .WithMany(u => u.StudentProjects)
+                .HasForeignKey(p => p.StudentId)
+                .HasPrincipalKey(u => u.UserId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            // Configure Project -> SupervisorProfile relationship
+            builder.Entity<Project>()
+                .HasOne(p => p.SupervisorProfile)
+                .WithMany(u => u.SupervisorProjects)
+                .HasForeignKey(p => p.SupervisorId)
+                .HasPrincipalKey(u => u.UserId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
     }
 }
