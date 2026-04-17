7e4e24d2a32213ea6568f45f7a7c591accc63101
bhanukaT56 <itsbhanukat56@gmail.com>
2026-04-17 23:52:38 +0530

    Add migration for StudentNumber and SupervisorNumber fields

---
+++ BlindMatchPAS/Data/Migrations/20260417182147_AddStudentAndSupervisorNumbers.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlindMatchPAS.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStudentAndSupervisorNumbers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "StudentNumber",
+                table: "UserProfiles",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SupervisorNumber",
+                table: "UserProfiles",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StudentNumber",
+                table: "UserProfiles");
+
+            migrationBuilder.DropColumn(
+                name: "SupervisorNumber",
+                table: "UserProfiles");
+        }
+    }
+}
