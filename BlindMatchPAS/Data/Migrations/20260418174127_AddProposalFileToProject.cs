9f61d6471dd277c349fa737dfe42dc929de9950f
bhanukaT56 <itsbhanukat56@gmail.com>
2026-04-18 23:12:19 +0530

    Add migration for ProposalFile field

---
+++ BlindMatchPAS/Data/Migrations/20260418174127_AddProposalFileToProject.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlindMatchPAS.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProposalFileToProject : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "ProposalFile",
+                table: "Projects",
+                type: "varbinary(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProposalFileName",
+                table: "Projects",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProposalFile",
+                table: "Projects");
+
+            migrationBuilder.DropColumn(
+                name: "ProposalFileName",
+                table: "Projects");
+        }
+    }
+}
